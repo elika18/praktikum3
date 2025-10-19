e90192b6a4146e7abd3bcc35ddebc2087d0f9a8f
elika18 <elmanueldmk@gmail.com>
2025-10-20 03:05:19 +0400

    Add files via upload

---
+++ hasil latihan3.py
@@ -0,0 +1,7 @@
+C:\Users\elika\PycharmProjects\praktikum3\.venv\Scripts\python.exe C:\Users\elika\PycharmProjects\praktikum3\latihan3.py
+Masukkan bilangan pertama: 4
+Masukkan bilangan kedua: 18
+Masukkan bilangan ketiga: 22
+Bilangan terbesar adalah: 22.0
+
+Process finished with exit code 0
